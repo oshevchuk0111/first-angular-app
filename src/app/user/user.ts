f85fd3f38adebcd6263d8173f869dfec51bf2265
oshevchuk0111 <o.shevchuk0111@gmail.com>
2026-01-22 21:31:41 +0100

    dynamic values with Angular

--- src/app/user/user.ts
+++ src/app/user/user.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 
+import { DUMMY_USERS } from '../dummy-users';
+
+const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+
 @Component({
   selector: 'app-user',
   imports: [],
@@ -7,5 +11,14 @@ import { Component } from '@angular/core';
   styleUrl: './user.scss',
 })
 export class UserComponent {
+  selectedUser = DUMMY_USERS[randomIndex];
 
+  get imagePath() {
+    return 'users/' + this.selectedUser.avatar
+  }
+
+  onSelectUser() {
+    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+    this.selectedUser = DUMMY_USERS[randomIndex];
+  }
 }
