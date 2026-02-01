b2df2fcf43c854e90e5629938323db09eb761d32
oshevchuk0111 <o.shevchuk0111@gmail.com>
2026-02-01 14:30:54 +0100

    service part1

--- src/app/tasks/tasks.ts
+++ src/app/tasks/tasks.ts
@@ -2,7 +2,7 @@ import { NewTaskComponent } from './new-task/new-task';
 import { TaskComponent } from './task/task';
 import { Component, Input } from '@angular/core';
 import { type NewTaskData } from './task/task.model';
-
+import { TasksService } from './tasks.service';
 @Component({
   selector: 'app-tasks',
   imports: [TaskComponent, NewTaskComponent],
@@ -13,37 +13,17 @@ export class TasksComponent {
   @Input({ required: true }) userId!: string;
   @Input({ required: true }) name!: string;
   isAddingTask = false;
-  tasks = [
-    {
-      id: 't1',
-      userId: 'u1',
-      title: 'Master Angular',
-      summary: 'Learn all the basic and advanced features of Angular & how to apply them.',
-      dueDate: '2026-02-28',
-    },
-    {
-      id: 't2',
-      userId: 'u3',
-      title: 'Build first prototype',
-      summary: 'Build a first prototype of the online shop website',
-      dueDate: '2026-05-31',
-    },
-    {
-      id: 't3',
-      userId: 'u3',
-      title: 'Prepare issue template',
-      summary: 'Prepare and describe an issue template which will help with project management',
-      dueDate: '2026-06-15',
-    },
-  ];
+  
+
+  constructor(private tasksService: TasksService) {
+    
+  }
 
   get selectedUserTasks() {
-    return this.tasks.filter((task) => task.userId === this.userId);
+    return this.tasksService.getUserTasks(this.userId);
   }
 
-  onCompleteTask(id: string) {
-    this.tasks = this.tasks.filter((task) => task.id !== id);
-  }
+  onCompleteTask(id: string) {}
 
   onStartAddTask() {
     this.isAddingTask = true;
@@ -53,14 +33,5 @@ export class TasksComponent {
     this.isAddingTask = false;
   }
 
-  onAddTask(taskData: NewTaskData) {
-    this.tasks.unshift({
-      id: new Date().getTime().toString(),
-      userId: this.userId,
-      title: taskData.title,
-      summary: taskData.summary,
-      dueDate: taskData.date,
-    });
-    this.isAddingTask = false;
-  }
+  onAddTask(taskData: NewTaskData) {}
 }
