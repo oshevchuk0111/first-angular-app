372932bc98757350eefefca53ade8486fbb80d75
oshevchuk0111 <o.shevchuk0111@gmail.com>
2026-02-01 13:31:31 +0100

    ng-content & pipe

--- src/app/tasks/task/task.ts
+++ src/app/tasks/task/task.ts
@@ -1,9 +1,11 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { DatePipe } from '@angular/common';
 
 import { type Task } from './task.model';
+import { CardComponent } from '../../shared/card/card';
 @Component({
   selector: 'app-task',
-  imports: [],
+  imports: [CardComponent, DatePipe],
   templateUrl: './task.html',
   styleUrl: './task.scss',
 })
